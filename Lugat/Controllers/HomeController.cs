8acd14ddbccf2aa7e15d799c0077042aed462bcb
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-12-26 18:47:02 +0500

    Code rub

--- Lugat/Controllers/HomeController.cs
+++ Lugat/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Lugat.Models.Foundations.Words;
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lugat.Controllers
