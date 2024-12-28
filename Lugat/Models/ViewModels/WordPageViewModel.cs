32dfd2f151cdd42e5f0497ebd92e0fe4974a3e84
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-12-28 20:48:30 +0500

    Part Five

---
+++ Lugat/Models/ViewModels/WordPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Lugat.Models.Foundations.Bolims;
+using Lugat.Models.Foundations.Words;
+
+namespace Lugat.Models.ViewModels
+{
+    public class WordPageViewModel
+    {
+        public int BolimId { get; set; }
+        public string BolimName { get; set; }
+        public IEnumerable<Word> Words { get; set; }
+    }
+}
