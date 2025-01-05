42c80df97a79ca8cc88b4749930dafa124e5c1ae
Javohir <103136749+Javohir14022000@users.noreply.github.com>
2025-01-05 21:43:34 +0500

    Fix

---
+++ Lugat/Models/ViewModels/BolimPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+using Lugat.Models.Foundations.Bolims;
+
+namespace Lugat.Models.ViewModels
+{
+    public class BolimPageViewModel
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public IEnumerable<Bolim> Bolims { get; set; }
+    }
+}
